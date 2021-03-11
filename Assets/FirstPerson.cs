cedc675ecd40e513d7bb751fc78cc94237ccc9aa
dkirk <dsak@bu.edu>
2021-03-10 19:37:52 -0500

    First commit

---
+++ Assets/FirstPerson.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// based on this tutorial: https://www.youtube.com/watch?v=_QajrabyTJc
+
+public class FirstPerson : MonoBehaviour
+{
+    public float mouseSensitivity = 300f;
+    public Transform playerBody;
+    public Transform playerCamera;
+    float xRotation = 0f;
+    float yRotation = 0f;
+
+    void Start() 
+    { 
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+ 
+    void Update ()
+    {
+        // get the mouse inputs
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        
+        // clamp x rotation
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+    
+        // rotate the camera
+        // yRotation += mouseX;
+        // playerBody.localRotation = Quaternion.Euler(0f, yRotation, 0f);
+        // playerCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    } 
+}
---
+++ Assets/PlayerMovement3D.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement3D : MonoBehaviour
+{
+    public float speed = 30;
+    public float moveSpeed = 10f;
+    private Vector3 motion;
+    private Rigidbody rb;
+
+    public Transform trackedObject;
+    private GameObject ahead;
+    private MeshRenderer _renderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        _renderer = trackedObject.gameObject.GetComponent<MeshRenderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        motion = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        rb.velocity = motion * speed * Time.deltaTime;
+        //KeyboardMovement();
+    }
+
+    void KeyboardMovement ()
+    {
+        Vector3 dir = new Vector3(0, 0, 0);
+    
+        dir.x = Input.GetAxis("Horizontal");
+        dir.z = Input.GetAxis("Vertical");
+    
+        transform.Translate(dir * moveSpeed * Time.deltaTime);
+    }
+}
